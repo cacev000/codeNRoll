8e2b81c4e26864b976ad3a110955a9060b4a0f27
florian lahitte <lahitte.florian@gmail.com>
2019-03-06 20:04:26 -0500

    Giorgo mistake

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/add", function(req, res) {
+app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
